b7f96ae6b4ef2b76565a13fc42057f3d4cedd17b
Alex Smirnov <alsmirnnov@gmail.com>
2012-04-03 01:16:41 -0700

    laplace uni and bigrams

--- pa2-autocorrect-v1/python/LaplaceBigramLanguageModel.py
+++ pa2-autocorrect-v1/python/LaplaceBigramLanguageModel.py
@@ -1,20 +1,50 @@
+import math, collections
+
+
 class LaplaceBigramLanguageModel:
 
   def __init__(self, corpus):
     """Initialize your data structures in the constructor."""
-    # TODO your code here
+    self.bigramms = collections.Counter()
+    self.words = collections.Counter()
+    self.total = 0
     self.train(corpus)
 
   def train(self, corpus):
     """ Takes a corpus and trains your language model. 
         Compute any counts or other corpus statistics in this function.
     """  
-    # TODO your code here
-    pass
+    for sentence in corpus.corpus:
+      data = sentence.data
+      for datum1,datum2 in zip(data,data[1:]):  
+        token1 = datum1.word
+        token2 = datum2.word
+        bg = BiGramm(token1,token2)
+        self.words[token1] += 1
+        self.bigramms[bg] += 1
+        self.total += 1
+ 
 
   def score(self, sentence):
     """ Takes a list of strings as argument and returns the log-probability of the 
         sentence using your language model. Use whatever data you computed in train() here.
     """
-    # TODO your code here
-    return 0.0
+    score = 0.0
+    for word1,word2 in zip(sentence,sentence[1:]):
+      bg = BiGramm(word1,word2)
+      count = self.bigramms[bg]
+      score += math.log(count+1)
+      score -= math.log(self.total) 
+    return score
+
+
+class BiGramm:
+  def __init__(self,first,second):
+    self.first = first
+    self.second = second
+
+  def __hash__(self):
+    return hash(self.first) + 31 * hash(self.second)
+
+  def __eq__(self,other):
+    return self.first == other.first and self.second == other.second
--- pa2-autocorrect-v1/python/LaplaceUnigramLanguageModel.py
+++ pa2-autocorrect-v1/python/LaplaceUnigramLanguageModel.py
@@ -1,20 +1,31 @@
+import math, collections
+
 class LaplaceUnigramLanguageModel:
 
   def __init__(self, corpus):
     """Initialize your data structures in the constructor."""
-    # TODO your code here
+    self.unigramCounts = collections.Counter()
+    self.total = 0
     self.train(corpus)
 
   def train(self, corpus):
     """ Takes a corpus and trains your language model. 
         Compute any counts or other corpus statistics in this function.
     """  
-    # TODO your code here
-    pass
+    for sentence in corpus.corpus:
+      for datum in sentence.data:  
+         token = datum.word
+         self.unigramCounts[token] += 1
+         self.total += 1
+    self.total += len(self.unigramCounts) 
 
   def score(self, sentence):
     """ Takes a list of strings as argument and returns the log-probability of the 
         sentence using your language model. Use whatever data you computed in train() here.
     """
-    # TODO your code here
-    return 0.0
+    score = 0.0 
+    for token in sentence:
+      count = self.unigramCounts[token]
+      score += math.log(count+1)
+      score -= math.log(self.total)
+    return score
